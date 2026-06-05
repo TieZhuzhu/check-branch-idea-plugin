0fb0df1a154c2eaab4c87146ba6192207733e64d
李澳 <836268029@qq.com>
2026-06-05 15:11:28 +0800

    fix(build): 修正版本号为正式版本
    - 将版本号从1.0.1-SNAPSHOT修改为1.0.2
    - 去除快照标识，准备发布正式版本
    - 保持其他构建配置不变

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.augustlee.tool"
-version = "1.0.1"
+version = "1.0.2"
 
 val localVerificationIdePath = providers.gradleProperty("localVerificationIdePath")
     .orElse(providers.environmentVariable("LOCAL_VERIFICATION_IDE_PATH"))
